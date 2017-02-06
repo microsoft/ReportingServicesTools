7a430dd01983a741a23e2c94706bda75f5cb9c27
Jaime Tarquino <jaimeta@microsoft.com>
2017-02-06 10:47:02 -0800

    updated alias

--- Functions/CatalogItems/New-RsFolder.ps1
+++ Functions/CatalogItems/New-RsFolder.ps1
@@ -13,26 +13,26 @@ function New-RsFolder
     .PARAMETER Path
         Specify the location where the folder should be created 
 
-    .PARAMETER FolderName
+    .PARAMETER Name
         Specify the name of the the new data folder
 
     .PARAMETER ReportServerUri
         Specify the Report Server URL to your SQL Server Reporting Services Instance.
 
-    .PARAMETER ReportServerCredentials
+    .PARAMETER Credential
         Specify the credentials to use when connecting to your SQL Server Reporting Services Instance.
 
     .PARAMETER Proxy
         Specify the Proxy to use when communicating with Reporting Services server. If Proxy is not specified, connection to Report Server will be created using ReportServerUri, ReportServerUsername and ReportServerPassword.
 
     .EXAMPLE 
-        New-RsFolder -Path '/' -FolderName 'My new folder'
+        New-RsFolder -Path '/' -Name 'My new folder'
         Description
         -----------
         This command will establish a connection to the Report Server located at http://localhost/reportserver using current user's credentials and create a new folder 'My new folder' at the root folder.
 
     .EXAMPLE 
-        New-RsFolder -ReportServerUri 'http://remoteServer/reportserver' -Path '/existingfolder' -FolderName 'My new folder'	
+        New-RsFolder -ReportServerUri 'http://remoteServer/reportserver' -Path '/existingfolder' -Name 'My new folder'	
         Description
         -----------
         This command will establish a connection to the Report Server located at http://remoteServer/reportserver using current user's credentials and create a new folder 'My new folder' at the folder existingfolder in the root.	
@@ -43,35 +43,37 @@ function New-RsFolder
     param
     (
         [Parameter(Mandatory=$True)]
+        [Alias('ItemPath')]
         [string]
         $Path,
 
         [Parameter(Mandatory=$True)]
+        [Alias('FolderName')]
         [string]
-        $FolderName,
+        $Name,
+
         [string]
         $ReportServerUri = 'http://localhost/reportserver',
 
         [System.Management.Automation.PSCredential]
-        $ReportServerCredentials,
+        $Credential,
 
         $Proxy
     )
 
     if (-not $Proxy)
     {
-        $Proxy = New-RSWebServiceProxy -ReportServerUri $ReportServerUri -Credentials $ReportServerCredentials
+        $Proxy = New-RSWebServiceProxy -ReportServerUri $ReportServerUri -Credentials $Credential
     }
 
     try
     {
         Write-Verbose "Creating folder..."
-        $Proxy.CreateFolder($FolderName, $Path, $null)
-        Write-Information "folder created successfully!"
+        $Proxy.CreateFolder($Name, $Path, $null)
+        Write-Information "Folder created successfully!"
     }
     catch
     {
-       Write-Error "Exception occurred while creating folder! $($_.Exception.Message)"
-       throw 
+       throw (New-Object System.Exception("Exception occurred while creating folder! $($_.Exception.Message)", $_.Exception))
     }
 }
