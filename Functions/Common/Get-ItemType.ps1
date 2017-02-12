07f5fd301af7f71849bdac243ccd00e8025efd1d
Aaron Nelson <SQLvariant@live.com>
2017-02-12 14:29:54 -0500

    Created Get-ItemType from logic in  Write-RsCatalogItem
    Pulled the logic for the Get-ItemType "utility" function out of
    Write-RsCatalogItem, converted it to a switch statement, and placed it
    in the Common folder.

---
+++ Functions/Common/Get-ItemType.ps1
@@ -0,0 +1,13 @@
+﻿function Get-ItemType
+{
+        param(
+            [string]$FileExtension
+        )
+switch ($FileExtension)
+    {
+        '.rdl'  {return 'Report'}
+        '.rsds' {return 'DataSource'}
+        '.rsd'  {return 'DataSet'}
+        default {throw 'Uploading currently only supports .rdl, .rsds and .rsd files'}
+    }
+}
