20d5124d3bf8f29aa5c40b1e648a26decfe2eca1
Erika Mart?nez <t-ermart@microsoft.com>
2017-03-12 12:58:03 -0500

    changes per comments

--- Tests/New-RsFolder.Tests.ps1
+++ Tests/New-RsFolder.Tests.ps1
@@ -3,7 +3,7 @@ Describe "New-RsFolder" {
         $folderName = 'SutFolderMinParameters' + [guid]::NewGuid()
         New-RsFolder -Path / -FolderName $folderName
         $folderList = Get-RsFolderContent -RsFolder /
-        $folderCount = ($folderList | where name -eq $folderName).count
+        $folderCount = ($folderList | where name -eq $folderName).Count
         It "Should be a new folder" {
             $folderCount | Should Be 1
         }
