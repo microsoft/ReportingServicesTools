d8d807248d8a558c6026184ea929e33f81b281ca
Erika Mart?nez <t-ermart@microsoft.com>
2017-03-12 12:05:32 -0500

    appveyor.yml modified for integration test

--- Tests/New-RsFolder.Tests.ps1
+++ Tests/New-RsFolder.Tests.ps1
@@ -1,6 +1,6 @@
 Describe "New-RsFolder" {
     Context "Creat Folder with minimun parameters"{
-        $folderName = 'SutFolderMinParameters'
+        $folderName = 'SutFolderMinParameters' + [guid]::NewGuid()
         New-RsFolder -Path / -FolderName $folderName
         $folderList = Get-RsFolderContent -RsFolder /
         $folderCount = ($folderList | where name -eq $folderName).count
