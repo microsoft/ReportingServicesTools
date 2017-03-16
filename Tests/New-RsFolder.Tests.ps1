40150ab522a7ea672540618a01c3031461a6ac5e
FriedrichWeinmann <friedrich.weinmann@powershell-stuttgart.de>
2017-03-16 22:12:57 +0100

    Fixed Test

---
+++ Tests/New-RsFolder.Tests.ps1
@@ -0,0 +1,11 @@
+﻿Describe "New-RsFolder" {
+    Context "Creat Folder with minimun parameters"{
+        $folderName = 'SutFolderMinParameters' + [guid]::NewGuid()
+        New-RsFolder -Path / -FolderName $folderName
+        $folderList = Get-RsFolderContent -Path /
+        $folderCount = ($folderList | Where-Object name -eq $folderName).Count
+        It "Should be a new folder" {
+            $folderCount | Should Be 1
+        }
+    }
+}
