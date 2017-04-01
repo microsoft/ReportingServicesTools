2085bc44a8ac9d1d510d4ad2ab63debb1903be4a
Erika Mart?nez <t-ermart@microsoft.com>
2017-04-01 09:17:35 -0500

    nothing change

--- Tests/CatalogItems/Out-RsFolderContent.Tests.ps1
+++ Tests/CatalogItems/Out-RsFolderContent.Tests.ps1
@@ -1,8 +1,8 @@
 # Copyright (c) 2016 Microsoft Corporation. All Rights Reserved.
 # Licensed under the MIT License (MIT)
 
-Describe "Write-RsFolderContent" {
-    Context "Write-RsFolderContent with min parameters"{
+Describe "Out-RsFolderContent" {
+    Context "Out-RsFolderContent with min parameters"{
         $dataSourceName = 'SutDataSourceMinParameters' + [guid]::NewGuid()
         $extension = 'SQL'
         $credentialRetrieval = 'None'
