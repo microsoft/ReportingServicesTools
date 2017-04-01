107b8fe9b879619de7b66b6e3254e9ce24bc48e8
Erika Mart?nez <t-ermart@microsoft.com>
2017-04-01 11:49:44 -0500

    Write-RsFolderContent updated with proxy, min and reportserverUri parameters

--- Tests/CatalogItems/Write-RsFolderContent.Tests.ps1
+++ Tests/CatalogItems/Write-RsFolderContent.Tests.ps1
@@ -4,22 +4,74 @@
 Describe "Write-RsFolderContent" {
     Context "Write-RsFolderContent with min parameters"{
         # First Local Folder
-        $localFolderName = 'SutLocalWrite_RsFolderContent' + [guid]::NewGuid()
-        $localPathFolder = (Get-Item -Path ".\" -Verbose).FullName + '\' + $localFolderName + '\' 
-        New-Item -Path '.' -Name $localFolderName  -ItemType "directory"
+        #$localFolderName = 'SutLocalWrite_RsFolderContent' + [guid]::NewGuid()
+        #$localPathFolder = (Get-Item -Path ".\" -Verbose).FullName + '\' + $localFolderName + '\' 
+        #New-Item -Path '.' -Name $localFolderName  -ItemType "directory"
         # RsFolder
         #$rsFolderName = 'SutWrite-RsFolderContentMinParameters' + [guid]::NewGuid()
         #$rsFolderPath = '/' + $rsFolderName 
         #New-RsFolder -Path / -FolderName $rsFolderName
-        $contentRsPath =  '/' + $localFolderName 
-        Write-RsFolderContent -Path $localPathFolder -RsFolder /
-        Get-RsFolderContent -RsFolder / -Recurse
-        $foundFolder = (Get-RsFolderContent -RsFolder / ) | Where-Object path -eq $contentRsPath
-        It "Should write a local folder in an RSFolder" {
-            $foundFolder.Count | Should Be 1 
+        $folderName = 'SutWriteRsFolderContentReportMin' + [guid]::NewGuid()
+        New-RsFolder -Path / -FolderName $folderName
+        $folderPath = '/' + $folderName
+        $localReportPath =   (Get-Item -Path ".\" -Verbose).FullName  + '\Tests\CatalogItems\testResources'
+        Write-RsFolderContent -Path $localReportPath -RsFolder $folderPath
+        $rsReportPath = $folderPath + '/' + $reportName
+        $uploadedReport = (Get-RsFolderContent -RsFolder / -Recurse ) | Where-Object path -eq $rsReportPath
+        It "Should upload a local report in Report Server" {
+            $uploadedReport.path | Should Be $rsReportPath
         }
         # Removing folders used for testing
-        #Remove-RsCatalogItem -RsFolder $rsFolderPath
+        Remove-RsCatalogItem -RsFolder $folderPath
         #Remove-Item $localPathFolder
+    }
+
+    Context "Write-RsFolderContent with ReportServerUri parameter"{
+        $folderName = 'SutWriteRsFolderContentReportMin' + [guid]::NewGuid()
+        New-RsFolder -Path / -FolderName $folderName
+        $folderPath = '/' + $folderName
+        $localReportPath =   (Get-Item -Path ".\" -Verbose).FullName  + '\Tests\CatalogItems\testResources'
+        $reportServerUri = 'http://localhost/reportserver'
+        Write-RsFolderContent -Path $localReportPath -RsFolder $folderPath -ReportServerUri $reportServerUri
+        $rsReportPath = $folderPath + '/' + $reportName
+        $uploadedReport = (Get-RsFolderContent -RsFolder / -Recurse ) | Where-Object path -eq $rsReportPath
+        It "Should upload a local report in Report Server with ReportServerUri Parameter" {
+            $uploadedReport.path | Should Be $rsReportPath
+        }
+        # Removing folders used for testing
+        Remove-RsCatalogItem -RsFolder $folderPath
+    }
+
+    Context "Write-RsFolderContent with Proxy Parameter"{
+        $folderName = 'SutWriteRsFolderContentReportMin' + [guid]::NewGuid()
+        New-RsFolder -Path / -FolderName $folderName
+        $folderPath = '/' + $folderName
+        $localReportPath =   (Get-Item -Path ".\" -Verbose).FullName  + '\Tests\CatalogItems\testResources'
+        $proxy = New-RsWebServiceProxy 
+        Write-RsFolderContent -Path $localReportPath -RsFolder $folderPath -Proxy $proxy
+        $rsReportPath = $folderPath + '/' + $reportName
+        $uploadedReport = (Get-RsFolderContent -RsFolder / -Recurse ) | Where-Object path -eq $rsReportPath
+        It "Should upload a local report in Report Server with Proxy Parameter" {
+            $uploadedReport.path | Should Be $rsReportPath
+        }
+        # Removing folders used for testing
+        Remove-RsCatalogItem -RsFolder $folderPath
+    }
+
+    Context "Write-RsFolderContent with Proxy and ReportServerUri"{
+        $folderName = 'SutWriteRsFolderContentReportMin' + [guid]::NewGuid()
+        New-RsFolder -Path / -FolderName $folderName
+        $folderPath = '/' + $folderName
+        $localReportPath =   (Get-Item -Path ".\" -Verbose).FullName  + '\Tests\CatalogItems\testResources'
+        $proxy = New-RsWebServiceProxy 
+        $reportServerUri = 'http://localhost/reportserver'
+        Write-RsFolderContent -Path $localReportPath -RsFolder $folderPath -Proxy $proxy -ReportServerUri $reportServerUri
+        $rsReportPath = $folderPath + '/' + $reportName
+        $uploadedReport = (Get-RsFolderContent -RsFolder / -Recurse ) | Where-Object path -eq $rsReportPath
+        It "Should upload a local report in Report Server with ReportServer and Proxy Uri" {
+            $uploadedReport.path | Should Be $rsReportPath
+        }
+        # Removing folders used for testing
+        Remove-RsCatalogItem -RsFolder $folderPath
     }
 }
