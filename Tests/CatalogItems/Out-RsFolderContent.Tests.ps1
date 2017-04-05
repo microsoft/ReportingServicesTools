37bba26b87f4aa4f8c0f0a6e44a9b78a6b4f1ed6
Erika Mart?nez <t-ermart@microsoft.com>
2017-04-05 06:36:35 -0500

    update Out-RsFolderContent to test to download a report and dataset

--- Tests/CatalogItems/Out-RsFolderContent.Tests.ps1
+++ Tests/CatalogItems/Out-RsFolderContent.Tests.ps1
@@ -3,24 +3,57 @@
 
 Describe "Out-RsFolderContent" {
     Context "Out-RsFolderContent with min parameters"{
-       
-            $dataSourceName = 'SutOutRsFolderContentDataSourceMinParam' + [guid]::NewGuid()
-            $extension = 'SQL'
-            $credentialRetrieval = 'None'
-            New-RsDataSource -RsFolder '/' -Name $dataSourceName -Extension $extension -CredentialRetrieval $credentialRetrieval
-            $dataSourcePath = '/' + $dataSourceName
-            $currentLocalPath = (Get-Item -Path ".\" -Verbose).FullName
-            Out-RsCatalogItem -RsFolder $dataSourcePath -Destination $currentLocalPath
-            $localDataSourceFile = $dataSourceName + '.rsds'
-            $localDataSourcePath = $currentLocalPath + '\' + $localDataSourceFile
-            Get-Item $localDataSourcePath
 
-            It "Should download a RsDataSource from Reporting Services with min parameters to a local folder" {
-            (Get-Item $localDataSourcePath).Name | Should Be $localDataSourceFile
-            }
-             # Removing folders used for testing
-            Remove-RsCatalogItem -RsFolder $dataSourcePath
-            Remove-Item  $localDataSourcePath
+        $folderName = 'SutWriteRsFolderContentMinParameters' + [guid]::NewGuid()
+        New-RsFolder -Path / -FolderName $folderName
+        $folderPath = '/' + $folderName
+        $localReportPath =   (Get-Item -Path ".\").FullName  + '\Tests\CatalogItems\testResources'
+        Write-RsFolderContent -Path $localReportPath -RsFolder $folderPath
+        $currentLocalPath = (Get-Item -Path ".\" ).FullName
+
+        It "Should download a Report from Reporting Services with min parameters to a local folder" {
+        $report = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'Report'
+        $reportPath = $folderPath + '/' + $report.Name 
+        Out-RsCatalogItem -RsFolder $reportPath -Destination $currentLocalPath
+        $localReportFile = $report.Name + '.rdl'
+        $localReportPath = $currentLocalPath + '\' + $localReportFile
+        Get-Item $localReportPath
+        (Get-Item $localReportPath).Name | Should Be $localDataSourceFile
+         }
+        # Removing local report downloaded while testing the command
+        Remove-Item  $localReportPath
+
+        It "Should download a Report from Reporting Services with min parameters to a local folder" {
+        $dataSet = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'DataSet'
+        $dataSetPath = $folderPath + '/' + $dataSet.Name 
+        Out-RsCatalogItem -RsFolder $dataSetPath -Destination $currentLocalPath
+        $localDataSetFile = $dataSet.Name + '.rdl'
+        $localDataSetPath = $currentLocalPath + '\' + $localDataSetFile
+        Get-Item $localDataSetPath
+        (Get-Item $localDataSetPath).Name | Should Be $localDataSetFile
+         }
+        # Removing folders used for testing in Report Server
+        Remove-RsCatalogItem -RsFolder $folderPath
+        # Removing local DataSet download while testing the command
+        Remove-Item  $localDataSetPath
+
+        It "Should download a RsDataSource from Reporting Services with min parameters to a local folder" {
+        $dataSourceName = 'SutOutRsFolderContentDataSourceMinParam' + [guid]::NewGuid()
+        $extension = 'SQL'
+        $credentialRetrieval = 'None'
+        New-RsDataSource -RsFolder '/' -Name $dataSourceName -Extension $extension -CredentialRetrieval $credentialRetrieval
+        $dataSourcePath = '/' + $dataSourceName
+        $currentLocalPath = (Get-Item -Path ".\" -Verbose).FullName
+        Out-RsCatalogItem -RsFolder $dataSourcePath -Destination $currentLocalPath
+        $localDataSourceFile = $dataSourceName + '.rsds'
+        $localDataSourcePath = $currentLocalPath + '\' + $localDataSourceFile
+        Get-Item $localDataSourcePath
+        (Get-Item $localDataSourcePath).Name | Should Be $localDataSourceFile
+        }
+        # Removing folders used for testing
+        Remove-RsCatalogItem -RsFolder $dataSourcePath
+        Remove-Item  $localDataSourcePath
+
     }
 
     Context "Out-RsFolderContent with ReportServerUri Parameter"{
