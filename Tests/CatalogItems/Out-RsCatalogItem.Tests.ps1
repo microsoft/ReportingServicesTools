4fd4cb364d4da2adda6e7827334002fe9b9ec972
Erika Mart?nez <t-ermart@microsoft.com>
2017-06-04 10:45:18 -0500

    corrections made on Out-RsCatalogItem test according to comments

--- Tests/CatalogItems/Out-RsCatalogItem.Tests.ps1
+++ Tests/CatalogItems/Out-RsCatalogItem.Tests.ps1
@@ -16,35 +16,35 @@ Describe "Out-RsCatalogItem" {
                 New-Item -Path $destinationPath -type "directory"
 
                 It "Should download a Report from Reporting Services with min parameters" {
-                # Download Report        
-                $report = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'Report'
-                $reportPath = $folderPath + '/' + $report.Name 
-                Out-RsCatalogItem -RsFolder $reportPath -Destination $destinationPath
-                # Test if the report was downloaded
-                $localReportDownloaded = Get-ChildItem  $localFolderName
-                $localReportDownloaded.Name | Should Be 'emptyReport.rdl'
-                $localReportDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' + 'emptyReport.rdl'
-                Remove-Item $localReportDownloadedPath
+                        # Download Report        
+                        $report = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'Report'
+                        $reportPath = $folderPath + '/' + $report.Name 
+                        Out-RsCatalogItem -RsFolder $reportPath -Destination $destinationPath
+                        # Test if the report was downloaded
+                        $localReportDownloaded = Get-ChildItem  $localFolderName
+                        $localReportDownloaded.Name | Should Be 'emptyReport.rdl'
+                        $localReportDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' + 'emptyReport.rdl'
+                        Remove-Item $localReportDownloadedPath
                 }
 
                 It "Should download a DataSet from Reporting Services with min parameters" {
-                $dataSet = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'DataSet'
-                $dataSetPath = $folderPath + '/' + $dataSet.Name 
-                Out-RsCatalogItem -RsFolder $dataSetPath -Destination $destinationPath
-                # Test if the dataset was downloaded
-                $localDataSetDownloaded = Get-ChildItem  $localFolderName
-                $localDataSetDownloaded.Name | Should Be 'UnDataset.rsd'
-                $localDataSetDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' + 'UnDataset.rsd'
-                Remove-Item $localDataSetDownloadedPath
+                        $dataSet = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'DataSet'
+                        $dataSetPath = $folderPath + '/' + $dataSet.Name 
+                        Out-RsCatalogItem -RsFolder $dataSetPath -Destination $destinationPath
+                        # Test if the dataset was downloaded
+                        $localDataSetDownloaded = Get-ChildItem  $localFolderName
+                        $localDataSetDownloaded.Name | Should Be 'UnDataset.rsd'
+                        $localDataSetDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' + 'UnDataset.rsd'
+                        Remove-Item $localDataSetDownloadedPath
                 }
                
                 It "Should download a RsDataSource from Reporting Services with min parameters" {
-                $dataSource = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'DataSource'
-                $dataSourcePath = $folderPath + '/' + $dataSource.Name 
-                Out-RsCatalogItem -RsFolder $dataSourcePath -Destination $destinationPath
-                $localDataSourceDownloaded = Get-ChildItem  $localFolderName
-                $localDataSourceDownloaded.Name | Should Be 'SutWriteRsFolderContent_DataSource.rsds'
-                $localDataSourceDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' +  'SutWriteRsFolderContent_DataSource.rsds'
+                        $dataSource = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'DataSource'
+                        $dataSourcePath = $folderPath + '/' + $dataSource.Name 
+                        Out-RsCatalogItem -RsFolder $dataSourcePath -Destination $destinationPath
+                        $localDataSourceDownloaded = Get-ChildItem  $localFolderName
+                        $localDataSourceDownloaded.Name | Should Be 'SutWriteRsFolderContent_DataSource.rsds'
+                        $localDataSourceDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' +  'SutWriteRsFolderContent_DataSource.rsds'
                   }
                 Remove-Item $destinationPath -Confirm:$false -Recurse
                 Remove-RsCatalogItem -RsFolder $folderPath
@@ -64,16 +64,16 @@ Describe "Out-RsCatalogItem" {
                 New-Item -Path $destinationPath -type "directory"
 
                 It "Should download a Report from Reporting Services with ReportServerUri Parameter" {
-                # Download Report        
-                $report = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'Report'
-                $reportServerUri = 'http://localhost/reportserver'
-                $reportPath = $folderPath + '/' + $report.Name 
-                Out-RsCatalogItem -RsFolder $reportPath -Destination $destinationPath -ReportServerUri $reportServerUri
-                # Test if the report was downloaded
-                $localReportDownloaded = Get-ChildItem  $localFolderName
-                $localReportDownloaded.Name | Should Be 'emptyReport.rdl'
-                $localReportDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' + 'emptyReport.rdl'
-                Remove-Item $localReportDownloadedPath
+                        # Download Report        
+                        $report = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'Report'
+                        $reportServerUri = 'http://localhost/reportserver'
+                        $reportPath = $folderPath + '/' + $report.Name 
+                        Out-RsCatalogItem -RsFolder $reportPath -Destination $destinationPath -ReportServerUri $reportServerUri
+                        # Test if the report was downloaded
+                        $localReportDownloaded = Get-ChildItem  $localFolderName
+                        $localReportDownloaded.Name | Should Be 'emptyReport.rdl'
+                        $localReportDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' + 'emptyReport.rdl'
+                        Remove-Item $localReportDownloadedPath
                 }
                 Remove-Item $destinationPath -Confirm:$false -Recurse
                 Remove-RsCatalogItem -RsFolder $folderPath
@@ -93,17 +93,17 @@ Describe "Out-RsCatalogItem" {
                 New-Item -Path $destinationPath -type "directory"
 
                 It "Should download a Report from Reporting Services with ReportServerUri and Proxy Parameter" {
-                # Download Report        
-                $report = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'Report'
-                $reportServerUri = 'http://localhost/reportserver'
-                $proxy = New-RsWebServiceProxy
-                $reportPath = $folderPath + '/' + $report.Name 
-                Out-RsCatalogItem -RsFolder $reportPath -Destination $destinationPath -ReportServerUri $reportServerUri -Proxy $proxy 
-                # Test if the report was downloaded
-                $localReportDownloaded = Get-ChildItem  $localFolderName
-                $localReportDownloaded.Name | Should Be 'emptyReport.rdl'
-                $localReportDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' + 'emptyReport.rdl'
-                Remove-Item $localReportDownloadedPath
+                        # Download Report        
+                        $report = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'Report'
+                        $reportServerUri = 'http://localhost/reportserver'
+                        $proxy = New-RsWebServiceProxy
+                        $reportPath = $folderPath + '/' + $report.Name 
+                        Out-RsCatalogItem -RsFolder $reportPath -Destination $destinationPath -ReportServerUri $reportServerUri -Proxy $proxy 
+                        # Test if the report was downloaded
+                        $localReportDownloaded = Get-ChildItem  $localFolderName
+                        $localReportDownloaded.Name | Should Be 'emptyReport.rdl'
+                        $localReportDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' + 'emptyReport.rdl'
+                        Remove-Item $localReportDownloadedPath
                 }
                 Remove-Item $destinationPath -Confirm:$false -Recurse
                 Remove-RsCatalogItem -RsFolder $folderPath
@@ -123,16 +123,16 @@ Describe "Out-RsCatalogItem" {
                 New-Item -Path $destinationPath -type "directory"
 
                 It "Should download a Report from Reporting Services with Proxy Parameter" {
-                # Download Report        
-                $report = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'Report'
-                $proxy = New-RsWebServiceProxy
-                $reportPath = $folderPath + '/' + $report.Name 
-                Out-RsCatalogItem -RsFolder $reportPath -Destination $destinationPath -Proxy $proxy 
-                # Test if the report was downloaded
-                $localReportDownloaded = Get-ChildItem  $localFolderName
-                $localReportDownloaded.Name | Should Be 'emptyReport.rdl'
-                $localReportDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' + 'emptyReport.rdl'
-                Remove-Item $localReportDownloadedPath
+                        # Download Report        
+                        $report = (Get-RsFolderContent -RsFolder $folderPath ) | Where-Object TypeName -eq 'Report'
+                        $proxy = New-RsWebServiceProxy
+                        $reportPath = $folderPath + '/' + $report.Name 
+                        Out-RsCatalogItem -RsFolder $reportPath -Destination $destinationPath -Proxy $proxy 
+                        # Test if the report was downloaded
+                        $localReportDownloaded = Get-ChildItem  $localFolderName
+                        $localReportDownloaded.Name | Should Be 'emptyReport.rdl'
+                        $localReportDownloadedPath = $currentLocalPath + '\' + $localFolderName +'\' + 'emptyReport.rdl'
+                        Remove-Item $localReportDownloadedPath
                 }
                 Remove-Item $destinationPath -Confirm:$false -Recurse
                 Remove-RsCatalogItem -RsFolder $folderPath
