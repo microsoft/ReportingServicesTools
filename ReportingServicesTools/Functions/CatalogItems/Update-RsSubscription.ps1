9fefa2841f113840b2015c51e92ba35df3e4a73a
Warren Estes <warren@warrenestes.com>
2017-09-30 13:34:20 -0400

    Add files via upload
    Added changes asked in PR

--- ReportingServicesTools/Functions/CatalogItems/Update-RsSubscription.ps1
+++ ReportingServicesTools/Functions/CatalogItems/Update-RsSubscription.ps1
@@ -6,11 +6,11 @@ function Update-RsSubscription
 {
   <#
         .SYNOPSIS
-            This script will update subscriptions piped from get-RsSubscriptions
+            This script will update subscriptions piped from Get-RsSubscriptions
 
         .DESCRIPTION
             This script will take the custom object producted by get-RSSubscription and use the data to update the 
-            matchdata xml to either a new startdatetime, or enddate.
+            matchdata xml to either a new startdatetime, enddate, or owner.
 
         .PARAMETER ReportServerUri
             Specify the Report Server URL to your SQL Server Reporting Services Instance.
@@ -25,24 +25,37 @@ function Update-RsSubscription
             Use "New-RsWebServiceProxy" to generate a proxy object for reuse.
             Useful when repeatedly having to connect to multiple different Report Server.
 
+        .PARAMETER StartDateTime
+            StartDateTime to change Start Date and Time of subscription.
+        
+        .PARAMETER EndDate
+            Used to change the EndDate of subscription. 
+
+        .PARAMETER Owner
+            Used to change the owner of a subscription. 
+            
+          
         .EXAMPLE
-            $proxy = "http://ssrs-t-1234/Reportserver"
-            Get-RsSubscription -ReportServerUri $proxy -path '/Finance/ImportantReports' | 
-            where description -eq 'super important' | Update-RsSubscription -ReportServerUri $proxy -EndDate 9/9/2099
+            Get-RsSubscription -path '/Finance/ImportantReports' | Update-RsSubscription -EndDate 9/9/2099
 
-            assign proxy to variable for reuse.
-            use get-rsssubscripion to find the 'super important' report in my /finance/importantreports path. Then Pipe the results
-            to update-RsSubscription where we reuse the same $proxy to update the EndDate to 9/9/2099.
+            Description
+            -----------
+            Update all subscriptions on localhost associated with '/finance/ImportantReports' to have an EndDate of 9/9/2099
 
         .EXAMPLE
-            $proxy = "http://ssrs-t-1234/Reportserver"
+            Get-RsSubscription -path '/Finance/ImportantReports' | Update-RsSubscription -StartDateTime "1/9/2017 9am"
 
-            [psobject]$subscriptions = Get-RsSubscription -ReportServerUri $proxy -path '/Finance/ImportantReports' | where description -eq 'super important' 
-            $subscriptions | Update-RsSubscription -ReportServerUri $proxy -EndDate 9/9/2099 -StartDate 1/1/2016
+            Description
+            -----------
+            Update all subscriptions on localhost associated with '/finance/ImportantReports' to have a startdate of 1/9/2017 and time of 9am
+        
+        .EXAMPLE
+            Get-RsSubscription -path '/Finance/ImportantReports' | Update-RsSubscription -Owner "Warren"
 
-            assign proxy to variable for reuse. 
-            Then create a psobject variable to store the objects created from get-RSSubscription.
-            Then Pipe the results to Update-RSSubscription where we reuse the same $proxy to update the EndDate to 9/9/2999 and StartDate to 1/1/2016
+            Description
+            -----------
+            Update all subscriptions on localhost associated with '/finance/ImportantReports' to have an owner of "Warren".
+            The user being updated needs to have the correct permissions in order to successfully update the owner.
         
     #>
     
@@ -50,15 +63,22 @@ function Update-RsSubscription
   param (
     [string]
     $ReportServerUri,
-    [Alias('ReportServerCredentials')]
+    
     [System.Management.Automation.PSCredential]
     $Credential,
+
     $Proxy,
+
     [parameter(Mandatory = $false)]
     [DateTime]$StartDateTime,
+
     [parameter(Mandatory = $false)]
     [DateTime]$EndDate,
-    [Parameter(Mandatory = $true, ValueFromPipeLine)]
+
+    [parameter(Mandatory = $False)]
+    [string]$Owner,
+
+    [Parameter(Mandatory=$true,ValueFromPipeLine)]
     [PSCustomObject[]]$SubProperties
         
   )
@@ -71,45 +91,50 @@ function Update-RsSubscription
   Process
   {
     Write-Verbose "Updating Subscriptions..."
-    try
-    {
-      [xml]$XMLMatch = $SubProperties.MatchData
+try
+ {
+   [xml]$XMLMatch = $SubProperties.MatchData
+
+    if ($owner){
+    $proxy.ChangeSubscriptionOwner($SubProperties.subscriptionID,$owner)
+    }
+
             
-      if ($StartDateTime)
-      {
+    if ($StartDateTime)
+    {
         $XMLMatch.ScheduleDefinition.StartDateTime.InnerText = $StartDateTime
-      }
+    }
     
-      if ($EndDate)
+    if ($EndDate)
+    {
+      #check to see if end date exists as a node
+      $EndExists = $XMLMatch.SelectNodes("//*") | Select-Object name | Where-Object name -eq "EndDate"
+      #if no enddate create child node
+      if ($EndExists -eq $null)
       {
-        #check to see if end date exists as a node
-        $EndExists = $XMLMatch.SelectNodes("//*") | Select-Object name | Where-Object name -eq "EndDate"
-        #if no enddate create child node
-        if ($EndExists -eq $null)
-        {
-          $child = $XMLMatch.CreateElement("EndDate")
-          $child.InnerText = $EndDate
+        $child = $XMLMatch.CreateElement("EndDate")
+        $child.InnerText = $EndDate
         
-          $XMLMatch.ScheduleDefinition.AppendChild($child)
+        $XMLMatch.ScheduleDefinition.AppendChild($child)
         
-        }
-        else
-        {
+      }
+      else
+      {
           #if enddate node exists update  
           $XMLMatch.ScheduleDefinition.EndDate.InnerText = $EndDate         
-        } 
+      } 
         
-      }
+    }
     
-      if ($StartDateTime -ne $null -or $EndDate -ne $null)
-      {
-        $null = $Proxy.SetSubscriptionProperties($SubProperties.subscriptionID, $SubProperties.DeliverySettings, $SubProperties.Description, $SubProperties.EventType, $XMLMatch.OuterXml, $SubProperties.Values) 
-        Write-Verbose "subscription $($SubProperties.subscriptionId) for $($SubProperties.report) report successfully updated!"
-      }
+    if ($StartDateTime -ne $null -or $EndDate -ne $null)
+    {
+      $null = $Proxy.SetSubscriptionProperties($SubProperties.subscriptionID, $SubProperties.DeliverySettings, $SubProperties.Description, $SubProperties.EventType, $XMLMatch.OuterXml, $SubProperties.Values) 
+      Write-Verbose "subscription $($SubProperties.subscriptionId) for $($SubProperties.report) report successfully updated!"
+    }
     }
     Catch
     {
-      throw (New-Object System.Exception("Exception while updating subscription(s)! $($_.Exception.Message)", $_.Exception))
+     throw (New-Object System.Exception("Exception while updating subscription(s)! $($_.Exception.Message)", $_.Exception))
     }
     
   }
