b3a0a21d6adcfe5b5379e0d983e8338d0b2a3279
Parth Shah <parthsha@live.com>
2017-11-04 18:37:33 -0700

    Removing option to overwrite when uploading an entire folder. Minor changes to Help Menu.

--- ReportingServicesTools/Functions/CatalogItems/Rest/Write-RsRestFolderContent.ps1
+++ ReportingServicesTools/Functions/CatalogItems/Rest/Write-RsRestFolderContent.ps1
@@ -5,26 +5,25 @@ function Write-RsRestFolderContent
 {
     <#
         .SYNOPSIS
-            This command uploads an item from disk to a report server. It is for SQL Server Reporting Service 2016 and later.
-        
+            This command uploads an item from disk to a report server.
+
         .DESCRIPTION
-            This command uploads an item from disk to a report server. It is for SQL Server Reporting Service 2016 and later. Currently, we only support uploading Reports, DataSources, DataSets and Mobile Reports.
-        
+            This command uploads an item from disk to a report server.
+
         .PARAMETER Path
             Path to item to upload on disk.
-        
+
         .PARAMETER RsFolder
             Folder on reportserver to upload the item to.
-
-        .PARAMETER Overwrite
-            Overwrite the old entry, if an existing catalog item with same name exists at the specified destination.
 
         .PARAMETER ReportPortalUri
             Specify the Report Portal URL to your SQL Server Reporting Services Instance.
 
         .PARAMETER RestApiVersion
-            Specify the version of REST Endpoint to use. Valid values are: "v1.0", "v2.0". 
-            NOTE: v1.0 of REST Endpoint is not supported by Microsoft.
+            Specify the version of REST Endpoint to use. Valid values are: "v1.0", "v2.0".
+            NOTE:
+                - v1.0 of REST Endpoint is not supported by Microsoft and is for SSRS 2016.
+                - v2.0 of REST Endpoint is supported by Microsoft and is for SSRS 2017, PBIRS October 2017 and newer releases.
 
         .PARAMETER Credential
             Specify the credentials to use when connecting to the Report Server.
@@ -60,7 +59,7 @@ function Write-RsRestFolderContent
             -----------
             Uploads the report 'monthlyreport.rdl' to folder '/monthlyreports' to v1.0 REST Endpoint located at http://myserver/reports.
     #>
-    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'Medium')]
+    [CmdletBinding()]
     param(
         [Parameter(Mandatory = $True, ValueFromPipeline = $true)]
         [string[]]
@@ -69,14 +68,9 @@ function Write-RsRestFolderContent
         [switch]
         $Recurse,
 
-        [Alias('DestinationFolder')]
         [Parameter(Mandatory = $True)]
         [string]
         $RsFolder,
-
-        [Alias('Override')]
-        [switch]
-        $Overwrite,
 
         [string]
         $ReportPortalUri,
@@ -132,7 +126,7 @@ function Write-RsRestFolderContent
                     $parentFolder = $RsFolder + $relativePath
                 }
 
-                New-RsRestFolder -WebSession $WebSession -RestApiVersion $RestApiVersion -FolderName $item.Name -RsFolder $parentFolder
+                New-RsRestFolder -WebSession $WebSession -RestApiVersion $RestApiVersion -FolderName $item.Name -RsFolder $parentFolder | Out-Null
             }
 
             if ($item.Extension -ne "")
