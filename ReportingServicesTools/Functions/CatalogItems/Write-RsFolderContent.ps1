16a5c5a336420e91993d1ec9b6ffb42f93d42dc7
Cláudio Silva <claudiosil100@gmail.com>
2018-05-27 17:49:39 +0100

    To support jpg/png when uploading whole folder

--- ReportingServicesTools/Functions/CatalogItems/Write-RsFolderContent.ps1
+++ ReportingServicesTools/Functions/CatalogItems/Write-RsFolderContent.ps1
@@ -7,39 +7,39 @@ function Write-RsFolderContent
     <#
         .SYNOPSIS
             Uploads all items in a folder on disk to a report server.
-        
+
         .DESCRIPTION
             Uploads all items in a folder on disk to a report server.
-            Currently, we are only supporting Report, DataSource and DataSet for uploads
-        
+            Currently, we are only supporting Report, DataSource, DataSet and jpg/png for uploads
+
         .PARAMETER Recurse
             A description of the Recurse parameter.
-        
+
         .PARAMETER Path
             Path to folder which contains items to upload on disk.
-        
+
         .PARAMETER RsFolder
             Folder on reportserver to upload the item to.
 
        .PARAMETER Overwrite
             Overwrite the old entry, if an existing catalog item with same name exists at the specified destination.
-        
+
         .PARAMETER ReportServerUri
             Specify the Report Server URL to your SQL Server Reporting Services Instance.
             Use the "Connect-RsReportServer" function to set/update a default value.
-        
+
         .PARAMETER Credential
             Specify the credentials to use when connecting to the Report Server.
             Use the "Connect-RsReportServer" function to set/update a default value.
-        
+
         .PARAMETER Proxy
             Report server proxy to use.
             Use "New-RsWebServiceProxy" to generate a proxy object for reuse.
             Useful when repeatedly having to connect to multiple different Report Server.
-        
+
         .EXAMPLE
             Write-RsFolderContent -ReportServerUri 'http://localhost/reportserver_sql2012' -Path c:\monthlyreports -RsFolder /monthlyReports
-            
+
             Description
             -----------
             Uploads all reports under c:\monthlyreports to folder /monthlyReports.
@@ -116,16 +116,19 @@ function Write-RsFolderContent
                     throw (New-Object System.Exception("Failed to create folder '$($item.Name)' in '$parentFolder': $($_.Exception.Message)", $_.Exception))
                 }
             }
-            
+
             if ($item.Extension -eq ".rdl" -or
                 $item.Extension -eq ".rsds" -or
                 $item.Extension -eq ".rsd" -or
-                $item.Extension -eq ".rds")
+                $item.Extension -eq ".rds" -or
+                $item.Extension -eq ".jpg" -or
+                $item.Extension -eq ".jpeg" -or
+                $item.Extension -eq ".png" )
             {
                 $relativePath = Clear-Substring -string $item.FullName -substring $sourceFolder.FullName.TrimEnd("\") -position front
                 $relativePath = Clear-Substring -string $relativePath -substring ("\" + $item.Name) -position back
                 $relativePath = $relativePath.replace("\", "/")
-                
+
                 if ($RsFolder -eq "/" -and $relativePath -ne "")
                 {
                     $parentFolder = $relativePath
