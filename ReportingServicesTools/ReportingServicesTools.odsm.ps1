44b634b1ef38bf05c65ee30b6ea56cb43009557d
McKie <Lloyd.McKie@justice.wa.gov.au>
2019-07-26 08:33:53 +0800

    Add in Octopus Deploy Export

---
+++ ReportingServicesTools/ReportingServicesTools.odsm.ps1
@@ -0,0 +1,9 @@
+﻿# Octopus deploy module export
+$mainScript = "$PSScriptRoot\ReportingServicesTools.Designer.ps1"
+@('# AUTO GENERATED SCRIPT') | sc -path $mainScript 
+gc "$PSScriptRoot\Libraries\library.ps1" | ac -path $mainScript
+
+$mainScript
+
+$scripts =  Get-ChildItem "$PSScriptRoot\Functions" -Recurse -Include *.ps1
+foreach ($script in $scripts) { gc $script | ac -path $mainScript }
