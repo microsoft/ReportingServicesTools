91835de209df7f4d4dcf3e306ecff32958e70161
Aaron Nelson <sqlvariant@live.com>
2020-05-22 14:19:22 -0400

    Update Get-RsRestFolderContent.Tests.ps1
    Added explicit call for the -reportPortalUri parameter to use the  $reportPortalUri variable, so that functions in the script are sure to be calling the same instance of SSRS/PBIRS.

--- Tests/CatalogItems/Rest/Get-RsRestFolderContent.Tests.ps1
+++ Tests/CatalogItems/Rest/Get-RsRestFolderContent.Tests.ps1
@@ -8,7 +8,7 @@ Describe "Get-RsRestFolderContent" {
     Context "Get folder with reportPortalUri parameter"{
         # Create a folder
         $folderName = 'SutGetFolderReportPortalUriParameter' + [guid]::NewGuid()
-        New-RsRestFolder -RsFolder / -FolderName $folderName
+        New-RsRestFolder-reportPortalUri $reportPortalUri -RsFolder / -FolderName $folderName
         $folderPath = '/' + $folderName
         # Test if the folder can be found
         $folderList = Get-RsRestFolderContent -reportPortalUri $reportPortalUri -RsFolder / 
@@ -23,7 +23,7 @@ Describe "Get-RsRestFolderContent" {
     Context "Get folder inside 4 folders"{
         # Create the first folder in the root
         $sutRootFolder = 'SutGetFolderParent' + [guid]::NewGuid()
-        New-RsRestFolder -RsFolder / -FolderName $sutRootFolder
+        New-RsRestFolder-reportPortalUri $reportPortalUri -RsFolder / -FolderName $sutRootFolder
         # Create 5 folders, one inside the other
         $currentFolderDepth = 2
         $folderParentName = $sutRootFolder
@@ -32,14 +32,14 @@ Describe "Get-RsRestFolderContent" {
             # Create a folder in a specified path 
             $folderParentPath +=  '/' + $folderParentName
             $folderParentName = 'SutGetFolderParent' + $currentFolderDepth 
-            New-RsRestFolder -RsFolder $folderParentPath -FolderName $folderParentName
+            New-RsRestFolder-reportPortalUri $reportPortalUri -RsFolder $folderParentPath -FolderName $folderParentName
             $currentFolderDepth +=1
             
         }
         # Test if the ´SutGetFolderParent5´ folder inside the other folders can be found
         $fifthFolderPath = $folderParentPath + '/' + $folderParentName
         $rootFolderPath = '/'  + $sutRootFolder 
-        $folderList = Get-RsRestFolderContent -RsFolder $rootFolderPath -Recurse
+        $folderList = Get-RsRestFolderContent-reportPortalUri $reportPortalUri -RsFolder $rootFolderPath -Recurse
         $folderCount = ($folderList | Where-Object path -eq $fifthFolderPath).Count
         It "Should found 4 subfolders" {
             $folderCount | Should Be 1
