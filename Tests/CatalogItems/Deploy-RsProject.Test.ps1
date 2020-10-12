f81c094cbd81793bb6161fa610d1c03cb3a56de0
Aaron Nelson <aaron.nelson@microsoft.com>
2020-10-12 15:13:32 -0400

    Changed location of TestProjects folder.

--- Tests/CatalogItems/Deploy-RsProject.Test.ps1
+++ Tests/CatalogItems/Deploy-RsProject.Test.ps1
@@ -1,26 +1,26 @@
 $reportPortalUri = if ($env:PesterPortalUrl -eq $null) { 'http://localhost/reports' } else { $env:PesterPortalUrl }
 
-Describe "Get-RsDeploymentConfig" {
-    Context "Get the DeploymentConfig of a ReportServer project file using ConfigurationToUse parameter"{
+Describe "Deploy-RsProject" {
+    Context "Deploy an entire SSRS Project by getting the DeploymentConfig of a ReportServer project file using ConfigurationToUse parameter"{
         # Create a folder
-        $RSConfig = Get-RsDeploymentConfig -RsProjectFile "$($PSScriptRoot)\testResources\TestProjects\SQLServerPerformanceDashboardReportingSolution\SQL Server Performance Dashboard\SQL Server Performance Dashboard.rptproj" -ConfigurationToUse Release |
+        $RSConfig = Get-RsDeploymentConfig -RsProjectFile "$($PSScriptRoot)\TestProjects\SQLServerPerformanceDashboardReportingSolution\SQL Server Performance Dashboard\SQL Server Performance Dashboard.rptproj" -ConfigurationToUse Release |
         Add-Member -PassThru -MemberType NoteProperty -Name ReportPortal -Value $reportPortalUri
         
         # Test if the config was retrieved
         It "Should verify a config was retrieved" {
-            @($RSConfig).Count | Should -Be 1
+            @($RSConfig).Count | Should Be 1
         }
         
         # Test if the TargetServerURL in the config was found
         It "Should verify TargetServerURL matches" {
-            $RSConfig.TargetServerURL | Should -Be 'http://localhost/reportserver'
+            $RSConfig.TargetServerURL | Should Be 'http://localhost/reportserver'
         }
 
         $RSConfig | Deploy-RsProject
         $CatalogList = Get-RsRestFolderContent -reportPortalUri $RSConfig.ReportPortal -RsFolder / -Recurse
         $folderCount = ($CatalogList | measure).Count
         It "Should find at least 1 folder" {
-            $folderCount | Should -Be 26
+            $folderCount | Should Be 25
         }
         # Removing folders used for testing
         Remove-RsCatalogItem -RsFolder $RSConfig.TargetReportFolder -Confirm:$false
--- Tests/CatalogItems/Get-RsDeploymentConfig.Tests.ps1
+++ Tests/CatalogItems/Get-RsDeploymentConfig.Tests.ps1
@@ -1,12 +1,12 @@
 
 Describe "Get-RsDeploymentConfig" {
+    $RSConfig = Get-RsDeploymentConfig -RsProjectFile "$($PSScriptRoot)\TestProjects\SQLServerPerformanceDashboardReportingSolution\SQL Server Performance Dashboard\SQL Server Performance Dashboard.rptproj" -ConfigurationToUse Release
+    
+    Write-Verbose "$RSConfig.TargetServerURL"
+    Write-Verbose "$RSConfig.RsProjectFolder"
+
     Context "Get the DeploymentConfig of a ReportServer project file using ConfigurationToUse parameter"{
         # Create a folder
-        $RSConfig = Get-RsDeploymentConfig -RsProjectFile "$($PSScriptRoot)\testResources\TestProjects\SQLServerPerformanceDashboardReportingSolution\SQL Server Performance Dashboard\SQL Server Performance Dashboard.rptproj" -ConfigurationToUse Release
-        
-        Write-Verbose "$RSConfig.TargetServerURL"
-        Write-Verbose "$RSConfig.RsProjectFolder"
-
         # Test if the config was found
         It "Should verify TargetServerURL matches" {
             $RSConfig.TargetServerURL | Should Be 'http://localhost/reportserver'
@@ -14,7 +14,7 @@ Describe "Get-RsDeploymentConfig" {
 
         # Test if the folder can be found
         It "Should verify a config was retrieved" {
-            $RSConfig.Count | Should Be 1
+            @($RSConfig).Count | Should Be 1
         }
         
     }
