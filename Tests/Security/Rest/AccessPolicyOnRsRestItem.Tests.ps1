fee3020bb89cfd5541a8a539e09fcbbad5742da4
Aaron Nelson <aaron.nelson@microsoft.com>
2021-02-04 15:43:06 -0500

    Update AccessPolicyOnRsRestItem.Tests.ps1
    Added tests for additional object types (Paginated & Power BI reports).

--- Tests/Security/Rest/AccessPolicyOnRsRestItem.Tests.ps1
+++ Tests/Security/Rest/AccessPolicyOnRsRestItem.Tests.ps1
@@ -42,4 +42,91 @@ Describe "Grant-RsRestItemAccessPolicy" {
         }
     }
 
+    Context "Get Catalog Item Policy"{
+
+        BeforeEach {
+            $session = New-RsRestSession -ReportPortalUri $reportPortalUri
+    
+            # creating test folder
+            $folderName = 'SUT_RevokeRsRestItemAccessPolicy_' + [guid]::NewGuid()
+            New-RsRestFolder -WebSession $session -RsFolder / -FolderName $folderName
+            $rsFolderPath = '/' + $folderName
+    
+            # uploading test artifacts: datasourcesReport.rdl and SqlPowerBIReport.pbix
+            $localPath =   (Get-Item -Path ".\").FullName  + '\Tests\CatalogItems\testResources'
+            Write-RsRestCatalogItem -WebSession $session -Path "$localPath\datasources\datasourcesReport.rdl" -RsFolder $rsFolderPath
+            $datasourcesReport =  "$rsFolderPath/datasourcesReport"
+        }
+
+        It "Should retrieve Browser permission access" {
+            $role = 'Browser'
+
+            # Grant Access
+            Grant-RsRestItemAccessPolicy -ReportPortalUri $ReportPortalUri -Identity $user -Role $role -Path $datasourcesReport -Verbose
+
+            $catalogItemPolicy = Get-RsRestItemAccessPolicy -ReportPortalUri $ReportPortalUri -Path $datasourcesReport
+            $catalogItemPolicyCount = @($catalogItemPolicy | Where-Object {$_.Identity -eq $user -and $_.Roles -eq $role}).Count
+            $catalogItemPolicyCount | Should BeGreaterThan 0
+        }
+
+        It "Should retrieve Content Manager permission access" {
+            $role = 'Content Manager'
+
+            # Grant Access
+            Grant-RsRestItemAccessPolicy -ReportPortalUri $ReportPortalUri -Identity $user -Role $role -Path $datasourcesReport -Verbose
+
+            $catalogItemPolicy = Get-RsRestItemAccessPolicy -ReportPortalUri $ReportPortalUri -Path $datasourcesReport
+            $catalogItemPolicyCount = @($catalogItemPolicy | Where-Object {$_.Identity -eq $user -and $_.Roles -eq $role}).Count
+            $catalogItemPolicyCount | Should BeGreaterThan 0
+        }
+
+        AfterEach {
+            Revoke-RsRestItemAccessPolicy -ReportPortalUri $ReportPortalUri -Identity $user -Path $datasourcesReport -Verbose
+        }
+    }
+
+        Context "Get Catalog Item Policy"{
+
+            BeforeEach {
+                $session = New-RsRestSession -ReportPortalUri $reportPortalUri
+        
+                # creating test folder
+                $folderName = 'SUT_RevokeRsRestItemAccessPolicy_' + [guid]::NewGuid()
+                New-RsRestFolder -WebSession $session -RsFolder / -FolderName $folderName
+                $rsFolderPath = '/' + $folderName
+        
+                # uploading test artifacts: datasourcesReport.rdl and SqlPowerBIReport.pbix
+                $localPath =   (Get-Item -Path ".\").FullName  + '\Tests\CatalogItems\testResources'
+        
+                Write-RsRestCatalogItem -WebSession $session -Path "$localPath\SqlPowerBIReport.pbix" -RsFolder $rsFolderPath
+                $sqlPowerBIReport = "$rsFolderPath/SqlPowerBIReport"
+            }
+    
+            It "Should retrieve Browser permission access" {
+            $role = 'Browser'
+
+            # Grant Access
+            Grant-RsRestItemAccessPolicy -ReportPortalUri $ReportPortalUri -Identity $user -Role $role -Path $sqlPowerBIReport -Verbose
+
+            $catalogItemPolicy = Get-RsRestItemAccessPolicy -ReportPortalUri $ReportPortalUri -Path $sqlPowerBIReport
+            $catalogItemPolicyCount = @($catalogItemPolicy | Where-Object {$_.Identity -eq $user -and $_.Roles -eq $role}).Count
+            $catalogItemPolicyCount | Should BeGreaterThan 0
+        }
+
+        It "Should retrieve Content Manager permission access" {
+            $role = 'Content Manager'
+
+            # Grant Access
+            Grant-RsRestItemAccessPolicy -ReportPortalUri $ReportPortalUri -Identity $user -Role $role -Path $sqlPowerBIReport -Verbose
+
+            $catalogItemPolicy = Get-RsRestItemAccessPolicy -ReportPortalUri $ReportPortalUri -Path $sqlPowerBIReport
+            $catalogItemPolicyCount = @($catalogItemPolicy | Where-Object {$_.Identity -eq $user -and $_.Roles -eq $role}).Count
+            $catalogItemPolicyCount | Should BeGreaterThan 0
+        }
+
+        AfterEach {
+            Revoke-RsRestItemAccessPolicy -ReportPortalUri $ReportPortalUri -Identity $user -Path $sqlPowerBIReport -Verbose
+        }
+    }
+
 }
