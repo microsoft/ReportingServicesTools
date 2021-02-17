cbed5210863d8b39d63570af2e28d2f7a09aa388
Aaron Nelson <aaron.nelson@microsoft.com>
2021-02-17 10:10:37 -0500

    Adding NULL handling for target folders (#320)
    * Adding NULL handling for target folders
    
    Adding NULL handling for Dataset, Datasource, & ReportPart folders.
    
    * Improved Pester test for Get-RsDeploymentConfig
    
    Added new deployment config to SQL Server Performance Dashboard.rptproj file. Improved Pester test for Get-RsDeploymentConfig to use new DebugNull deployment config.

--- Tests/CatalogItems/Get-RsDeploymentConfig.Tests.ps1
+++ Tests/CatalogItems/Get-RsDeploymentConfig.Tests.ps1
@@ -4,17 +4,56 @@ Describe "Get-RsDeploymentConfig" {
     
     Write-Verbose "$RSConfig.TargetServerURL"
     Write-Verbose "$RSConfig.RsProjectFolder"
+    Write-Verbose "$RSConfig.TargetDatasetFolder"
+    Write-Verbose "$RSConfig.TargetDatasourceFolder"
+    Write-Verbose "$RSConfig.TargetReportPartFolder"
 
-    Context "Get the DeploymentConfig of a ReportServer project file using ConfigurationToUse parameter"{
-        # Create a folder
-        # Test if the config was found
+    Context "Get the Release DeploymentConfig of a ReportServer project file using ConfigurationToUse parameter"{
         It "Should verify TargetServerURL matches" {
             $RSConfig.TargetServerURL | Should Be 'http://localhost/reportserver'
         }
 
-        # Test if the folder can be found
         It "Should verify a config was retrieved" {
             @($RSConfig).Count | Should Be 1
+        }
+
+        It "Should verify the Dataset, Datasource, and ReportPart folders are NULL" {
+            #$RSConfig = Get-RsDeploymentConfig -RsProjectFile "$($PSScriptRoot)\TestProjects\SQLServerPerformanceDashboardReportingSolution\SQL Server Performance Dashboard\SQL Server Performance Dashboard.rptproj" -ConfigurationToUse Release
+            
+            Write-Verbose "$RSConfig.TargetDatasetFolder"
+            Write-Verbose "$RSConfig.TargetDatasourceFolder"
+            Write-Verbose "$RSConfig.TargetReportPartFolder"
+
+            $RSConfig.TargetDatasetFolder | Should Be '/Datasets'
+            $RSConfig.TargetDatasourceFolder | Should Be '/Data Sources'
+            $RSConfig.TargetReportPartFolder | Should Be 'Report Parts'
+        }
+        
+    }
+
+    Context "Get the DebugNull DeploymentConfig of a ReportServer project file using ConfigurationToUse parameter"{
+        It "Should verify TargetServerURL matches" {
+            $RSConfig = Get-RsDeploymentConfig -RsProjectFile "$($PSScriptRoot)\TestProjects\SQLServerPerformanceDashboardReportingSolution\SQL Server Performance Dashboard\SQL Server Performance Dashboard.rptproj" -ConfigurationToUse DebugNull
+            
+            Write-Verbose "$RSConfig.TargetServerURL"
+            Write-Verbose "$RSConfig.RsProjectFolder"
+            Write-Verbose "$RSConfig.TargetDatasetFolder"
+            Write-Verbose "$RSConfig.TargetDatasourceFolder"
+            Write-Verbose "$RSConfig.TargetReportPartFolder"
+
+            $RSConfig.TargetServerURL | Should Be 'http://localhost/reportserver'
+        }
+
+        It "Should verify a config was retrieved" {
+            @($RSConfig).Count | Should Be 1
+        }
+
+        It "Should verify config was successfully retrieved even though Dataset, Datasource, and ReportPart folders are NULL" {
+            $RSConfig = Get-RsDeploymentConfig -RsProjectFile "$($PSScriptRoot)\TestProjects\SQLServerPerformanceDashboardReportingSolution\SQL Server Performance Dashboard\SQL Server Performance Dashboard.rptproj" -ConfigurationToUse DebugNull
+            
+            $RSConfig.TargetDatasetFolder | Should BeNullOrEmpty
+            $RSConfig.TargetDatasourceFolder | Should BeNullOrEmpty
+            $RSConfig.TargetReportPartFolder | Should BeNullOrEmpty
         }
         
     }
