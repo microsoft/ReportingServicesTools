e41048c95cad790e592768754c20bba2beafdcd0
Doug F <43570139+Periscope-NZ@users.noreply.github.com>
2023-06-27 01:58:45 +1200

    Add files via upload (#408)
    Add .xlsm (Microsoft Excel macro-enabled spreadsheet) to the type lookup. XLSM files are supported by Power BI but until now they could not be uploaded, refer to issue #349

--- ReportingServicesTools/Functions/Common/Get-ItemType.ps1
+++ ReportingServicesTools/Functions/Common/Get-ItemType.ps1
@@ -6,15 +6,16 @@
     )
     switch ($FileExtension)
     {
-        '.rdl'  { return 'Report' }
-        '.rsds' { return 'DataSource' }
-        '.rds' { return 'DataSource' }
-        '.rsd'  { return 'DataSet' }
+        '.rdl'      { return 'Report' }
+        '.rsds'     { return 'DataSource' }
+        '.rds'      { return 'DataSource' }
+        '.rsd'      { return 'DataSet' }
         '.rsmobile' { return 'MobileReport' }
-        '.pbix' { return 'PowerBIReport' }
-        '.xls' { return 'ExcelWorkbook' }
-        '.xlsx' { return 'ExcelWorkbook' }
-        '.kpi' { return 'Kpi' }
-        default { return 'Resource' }
+        '.pbix'     { return 'PowerBIReport' }
+        '.xls'      { return 'ExcelWorkbook' }
+        '.xlsx'     { return 'ExcelWorkbook' }
+        '.xlsm'     { return 'ExcelWorkbook' }
+        '.kpi'      { return 'Kpi' }
+        default     { return 'Resource' }
     }
 }
