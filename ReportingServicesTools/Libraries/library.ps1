e78a40e8f9c858fee3a7902002c656cff84b59af
Qi Zhang <50432983+Zhangqi910@users.noreply.github.com>
2025-02-20 09:49:14 +0000

    fix build error

--- ReportingServicesTools/Libraries/library.ps1
+++ ReportingServicesTools/Libraries/library.ps1
@@ -119,7 +119,7 @@ namespace Microsoft.ReportingServicesTools
         /// <summary>
         /// SQL Server 2022
         /// </summary>
-        SQLServer2022 = 16
+        SQLServer2022 = 16,
 
         /// <summary>
         /// Power BI Report Server
@@ -130,4 +130,4 @@ namespace Microsoft.ReportingServicesTools
 "@
 
 Try { Add-Type -TypeDefinition $source -ErrorAction Stop }
-catch { }
+catch { Write-Error "Error: $_" }
